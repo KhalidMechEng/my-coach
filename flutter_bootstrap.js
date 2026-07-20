9d0a9c5f48ed05c2a66ce55b1404242116ac23b9
Khalid Al-Shaqsi <khalidal-shaqsi@MacBook-Pro-Khalid.local>
2026-07-20 12:52:00 +0400

    Deploy: Sun–Thu week + Thursday cardio 2026-07-20

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "592665922" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3096343550" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
